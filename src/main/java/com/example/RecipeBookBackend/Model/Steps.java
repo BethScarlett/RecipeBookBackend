6db7d37be2bed9a01831bb5c33c5ce4ccc31e6f8
Bethany Burrows <jedcburrows@gmail.com>
2024-06-05 17:25:18 +0100

    Added models for ingredient and steps tables.

---
+++ src/main/java/com/example/RecipeBookBackend/Model/Steps.java
@@ -0,0 +1,51 @@
+package com.example.RecipeBookBackend.Model;
+
+import jakarta.persistence.*;
+
+@Entity
+public class Steps {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    private int stepNumber;
+    private String step;
+
+    @ManyToOne
+    @JoinColumn(name = "recipe_id")
+    private Recipe recipe;
+
+    public Steps() {
+
+    }
+
+    public Steps(long id, int stepNumber, String step) {
+        this.id = id;
+        this.stepNumber = stepNumber;
+        this.step = step;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public int getStepNumber() {
+        return stepNumber;
+    }
+
+    public void setStepNumber(int stepNumber) {
+        this.stepNumber = stepNumber;
+    }
+
+    public String getStep() {
+        return step;
+    }
+
+    public void setStep(String step) {
+        this.step = step;
+    }
+}
